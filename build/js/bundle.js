513e42144c1dace7689574bd78c1cc561a55a74d
辛忠柱 <xzzw9987@gmail.com>
2016-01-11 17:25:43 +0800

    ci

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -48,23 +48,6 @@
 
 	var _Object$defineProperties = __webpack_require__(1)['default'];
 
-	(function () {
-	    var query = parse(location.search.substring(1));
-	    var appid = 'wx682926e9be5cf5c5';
-	    if (!query['openid'] || !query['access_token']) location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appid + '&redirect_uri='
-	    // @todo change url here
-	     + 'http://wx.wine-dynasty.com/mjcq/proxy.html?url=' + encodeURIComponent(location.href) + '&response_type=code&scope=snsapi_userinfo&state=0#wechat_redirect';
-
-	    function parse(s) {
-	        if (!s) return {};
-	        var o = {};
-	        s.split('&').forEach(function (el) {
-	            o[el.split('=')[0]] = el.split('=')[1];
-	        });
-	        return o;
-	    }
-	})();
-
 	var query = __webpack_require__(4)(location.search.substring(1));
 	var access_token = query.access_token;
 	var openid = query.openid;
--- js/app.js
+++ js/app.js
@@ -1,25 +1,3 @@
-(function () {
-    var query = parse(location.search.substring(1));
-    var appid = 'wx682926e9be5cf5c5';
-    if (!query['openid'] || !query['access_token'])
-        location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='
-            + appid
-            + '&redirect_uri='
-                // @todo change url here
-            + 'http://wx.wine-dynasty.com/mjcq/proxy.html?url=' + encodeURIComponent(location.href)
-            + '&response_type=code&scope=snsapi_userinfo&state=0#wechat_redirect';
-
-    function parse(s) {
-        if (!s)
-            return {};
-        var o = {};
-        s.split('&').forEach(function (el) {
-            o[el.split('=')[0]] = el.split('=')[1];
-        });
-        return o;
-    }
-})();
-
 var query = require('./query')(location.search.substring(1));
 var {access_token,openid} = query;
 // GET method
