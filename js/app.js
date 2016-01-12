392e518bf5f973ba19fd7a5be117f214cd5e2b09
辛忠柱 <xzzw9987@gmail.com>
2016-01-12 19:35:43 +0800

    ci

--- js/app.js
+++ js/app.js
@@ -260,13 +260,28 @@ $(document).on('touchend', '.s-hint', function () {
 $(document).on('touchend', '.rl-btn', function () {
     $('.rules').toggle();
 });
-alert('debug');
+
 wx.ready(function () {
 
     wx.onMenuShareTimeline({
         title: '王朝酒业', // 分享标题
         link: 'http://wx.wine-dynasty.com/mjcq/index.html', // 分享链接
         imgUrl: 'http://wx.wine-dynasty.com/mjcq/fg.jpg', // 分享图标
+        success: function () {
+            // 用户确认分享后执行的回调函数
+        },
+        cancel: function () {
+            // 用户取消分享后执行的回调函数
+        }
+    });
+
+    wx.onMenuShareAppMessage({
+        title: '王朝酒业', // 分享标题
+        desc: '王朝酒业', // 分享描述
+        link: 'http://wx.wine-dynasty.com/mjcq/index.html', // 分享链接
+        imgUrl: 'http://wx.wine-dynasty.com/mjcq/fg.jpg', // 分享图标
+        type: 'link', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         success: function () {
             // 用户确认分享后执行的回调函数
         },
