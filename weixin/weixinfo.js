25958aa8ae9bc9fcf1007198675384cafecfd9a7
辛忠柱 <xzzw9987@gmail.com>
2016-01-13 15:02:42 +0800

    ci

--- weixin/weixinfo.js
+++ weixin/weixinfo.js
@@ -5,7 +5,7 @@ var querystring = require('querystring');
 var processArgs = {};
 var nonceStr = 'abqqvzpe';
 
-[process.argv[2], process.argv[3]].forEach(el=> {
+[process.argv[2], process.argv[3]].forEach(function (el) {
     if (el)
         processArgs[el.split('=')[0]] = el.split('=')[1];
 });
@@ -25,13 +25,13 @@ function getAccessToken() {
     https.get({
         hostname: 'api.weixin.qq.com',
         path: '/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret
-    }, res=> {
+    }, function (res) {
         var d = '';
         res.setEncoding('utf8');
-        res.on('data', data=> {
+        res.on('data', function (data) {
             d += data;
         });
-        res.on('end', ()=> {
+        res.on('end', function () {
             d = JSON.parse(d);
             accessToken = d['access_token'];
             if (accessToken)
@@ -47,13 +47,13 @@ function getJSAPITicket() {
             hostname: 'api.weixin.qq.com',
             path: '/cgi-bin/ticket/getticket?access_token=' + accessToken + '&type=jsapi'
         },
-        res=> {
+        function (res) {
             var d = '';
             res.setEncoding('utf8');
-            res.on('data', data=> {
+            res.on('data', function (data) {
                 d += data;
             });
-            res.on('end', ()=> {
+            res.on('end', function () {
                 d = JSON.parse(d);
                 jsApiTicket = d['ticket'];
             })
@@ -82,7 +82,7 @@ function getSignature(url) {
         .digest('hex');
 }
 
-module.exports = http.createServer((req, res)=> {
+module.exports = http.createServer(function (req, res) {
     // get /config?url=encodeURIComponent()
     var content = '';
     // Routers
@@ -93,16 +93,15 @@ module.exports = http.createServer((req, res)=> {
             'content-type': 'application/javascript; charset=utf-8'
         });
         var sig = getSignature(q.url);
-        content = `
-        wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '${appid}', // 必填，公众号的唯一标识
-    timestamp: ${timestamp}, // 必填，生成签名的时间戳
-    nonceStr: '${nonceStr}', // 必填，生成签名的随机串
-    signature: '${sig}',// 必填，签名，见附录1
-    jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
-    `;
+        content =
+            "wx.config({" +
+            "debug: false, " +
+            "appId: '" + appid + "'," +
+            "timestamp: '" + timestamp + "'," +
+            "nonceStr: '" + nonceStr + "'," +
+            "signature: '" + sig + "'," +
+            "jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage']" +
+            '})';
         console.log(content);
         res.end(content);
 
@@ -120,7 +119,7 @@ module.exports = http.createServer((req, res)=> {
             https.get({
                 hostname: 'api.weixin.qq.com',
                 path: '/sns/userinfo?access_token=' + at + '&openid=' + openid + '&lang=zh_CN'
-            }, _res=> {
+            }, function (_res) {
                 _res.pipe(res);
             });
         }
@@ -139,13 +138,13 @@ module.exports = http.createServer((req, res)=> {
             https.get({
                 hostname: 'api.weixin.qq.com',
                 path: '/sns/oauth2/access_token?appid=' + appid + '&secret=' + secret + '&code=' + code + '&grant_type=authorization_code'
-            }, _res=> {
+            }, function (_res) {
                 var d = '';
                 _res.setEncoding('utf8');
-                _res.on('data', data=> {
+                _res.on('data', function (data) {
                     d += data;
                 });
-                _res.on('end', ()=> {
+                _res.on('end', function () {
                     d = JSON.parse(d);
                     u = decodeURIComponent(u);
                     res.writeHead(302, {
