d4ee79f7645935bb0e6dfabf0f984766d3e7e708
辛忠柱 <xzzw9987@gmail.com>
2016-01-13 15:26:05 +0800

    ci

--- weixin/weixin.js
+++ weixin/weixin.js
@@ -1,4 +1,4 @@
-//var WeiXinInfoServer = require('./weixinfo');
-var WeixinAwardServer = require('./weixindb');
-//WeiXinInfoServer.listen(12000);
-WeixinAwardServer.listen(13000);
+var WeiXinInfoServer = require('./weixinfo');
+//var WeixinAwardServer = require('./weixindb');
+WeiXinInfoServer.listen(12000);
+//WeixinAwardServer.listen(13000);
