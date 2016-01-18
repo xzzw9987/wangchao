0d8eb0fb2f9b99ba9f248f99341b44af8326e215
辛忠柱 <xzzw9987@gmail.com>
2016-01-18 12:40:44 +0800

    ci

--- js/app.js
+++ js/app.js
@@ -207,7 +207,7 @@ function res(d) {
             return `
                 <div class="z zj2 ">
                     <div class="logo"></div>
-                    <div style="position: absolute;left: 112px;top: 220px;font-size: 30px;color: #a60006;">
+                    <div style="position: absolute;left: 120px;top: 220px;font-size: 30px;color: #a60006;">
                         恭喜${d.size}元红包已存入账户!
                     </div>
                     <img src="./erweima.png" width="640">
